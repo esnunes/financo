b91d1aba30af605b7bfe1127cf656979feaadc32
Eduardo Nunes <esnunes@gmail.com>
2019-03-18 17:45:42 +0100

    Move require optparse to parser file

--- lib/financo/cli/program.rb
+++ lib/financo/cli/program.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'optparse'
-
 module Financo
   module CLI
     # Program
--- lib/financo/cli/program/parser.rb
+++ lib/financo/cli/program/parser.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 module Financo
   module CLI
     class Program
